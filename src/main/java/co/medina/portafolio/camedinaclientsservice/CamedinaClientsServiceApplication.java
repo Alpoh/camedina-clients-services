a020d4c73a0b8278ef1cc0eedaebbb4edd44b98a
Camilo Medina <medina1camilo@gmail.com>
2026-08-04 22:21:56 +0200

    [master] baseline

---
+++ src/main/java/co/medina/portafolio/camedinaclientsservice/CamedinaClientsServiceApplication.java
@@ -0,0 +1,13 @@
+package co.medina.portafolio.camedinaclientsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamedinaClientsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamedinaClientsServiceApplication.class, args);
+    }
+
+}
